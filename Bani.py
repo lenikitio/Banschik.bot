b2747b65555afda0d5c691d2b4050b79da1a7156
nikita <tolstoguzov.nikita98@mail.ru>
2024-01-19 09:38:27 +0300

    Создание класса бани

---
+++ Bani.py
@@ -0,0 +1,34 @@
+class Bani:
+
+
+    # Конструктор
+    def __init__(
+            self, 
+            name: str, 
+            vastness: int, bani, sauna, hamam, storage, inovation, food, bar, decor, clean, pool, service, crayfish, price, authenticity, entertainment):
+        self.name = name
+        self.vastness = vastness
+        self.bani = bani
+        self.sauna = sauna
+        self.hamam = hamam
+        self.storage = storage
+        self.inovation = inovation
+        self.food = food
+        self.bar = bar
+        self.decor = decor
+        self.clean = clean
+        self.pool = pool
+        self.service = service
+        self.crayfish = crayfish
+        self.price = price
+        self.authenticity = authenticity
+        self.entertainment = entertainment
+        
+
+
+
+
+
+
+
+        
