858e5521496c06dc3f84ad430054bfc0f9cdd14a
nikita <tolstoguzov.nikita98@mail.ru>
2024-03-08 17:58:37 +0300

    Third commit

--- Banschik.py
+++ Banschik.py
@@ -6,7 +6,7 @@ from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, filte
 from telegram.ext._utils.types import FilterDataDict
 from telegram.ext.filters import MessageFilter
 from config import parol
-from telegram.ext import ConversationHandler
+from telegram.ext import ConversationHandler, CallbackQueryHandler
 
 
 # Наш логгер который записывает данные каждые 10 секунд
@@ -22,16 +22,26 @@ test_bani = {}
 # Метод команды /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
-        [InlineKeyboardButton("Банная оценка", callback_data="like-trex")],
-        [InlineKeyboardButton("Вывести Банную Базу", callback_data="like-trex")]
+        [InlineKeyboardButton("Банная оценка", callback_data="test")],
+        [InlineKeyboardButton("Вывести Банную Базу", callback_data="base")]
     ]
     await update.message.reply_text(text= "Здарова мужики, чего нужно", reply_markup= InlineKeyboardMarkup(keyboard))
 
+async def start_test(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # Убираем кнопки
+    await update.callback_query.message.edit_reply_markup(None)
 
+    # Меняем текст сообщения
+    text_сhat = "Запрос принят, в личной беседе о бане и поговорим"
+    await update.callback_query.edit_message_text(text_сhat)
+    # Отправляем сообщение в личный чат
+    text_id = "Напиши команду /test и начнём"
+    await context.bot.send_message(update.effective_user.id, text_id)
+    
+    
 # Тест
 async def test(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     user_id = update.effective_user.id
-    await context.bot.send_message(chat_id = update.effective_chat.id, text = "Ну начнём опрос")
     await context.bot.send_message(chat_id = user_id, text= "И в каких банях были?")
     return 1
 
@@ -57,11 +67,15 @@ if __name__ == '__main__':
     handler = ConversationHandler(
         [CommandHandler("test", test)],
         {
-            1: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_baniname)]
+            1 : [MessageHandler(filters.TEXT & ~filters.COMMAND, get_baniname)]
         },
         fallbacks=[]
     )
+
     application.add_handler(handler)
+
+    # Наш обработчит inline кнопок (тут кнопки с callback = "test")
+    application.add_handler(CallbackQueryHandler(start_test, "test"))
   
     # Команда, которая непосредственно запускает нашего бота и держит живым, нажав сочетание Ctrl + C остановит его
     application.run_polling()
